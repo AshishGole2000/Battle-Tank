1d0ede7471655846d093087c86802f1cd66b8dd0
Ashish Gole <ashishmgole@gmail.com>
2023-01-22 11:41:23 +0530

    Initial commit

---
+++ Assets/Scripts/TankSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class TankSpawner : MonoBehaviour
+{
+    [System.Serializable]
+    public class Tank
+    {
+        public float movementSpeed;
+        public float rotationSpeed;
+        public TankTypes tankType;
+        public Material color;
+        public ShellScript shellPrefab;
+    }
+
+    public CameraController cam;
+
+    public List<Tank> tankList;
+
+    public TankView tankView;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    public void CreateTank(TankTypes tankType)
+    {
+        int index = (int)tankType;
+        TankModel tankModel = new TankModel(tankList[index].movementSpeed, tankList[index].rotationSpeed, tankList[index].tankType, tankList[index].color, tankList[index].shellPrefab);
+        TankController tankController = new TankController(tankModel, tankView, cam);
+
+    }
+}
---
+++ Assets/Scripts/TankView.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TankView : MonoBehaviour
+{
+
+    private TankController tankController;
+    private float movementInput;
+    private float rotationInput;
+
+    public Transform firePoint;
+    public Rigidbody rb;
+    public MeshRenderer[] childs;
+
+    public AudioClip driving;
+    public AudioClip idle;
+    public AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+
+        if (movementInput != 0)
+            tankController.Move(movementInput);
+
+        if (rotationInput != 0)
+            tankController.Rotate(rotationInput);
+        PlayMovementAudio();
+        CheckFiring();
+    }
+
+	private void PlayMovementAudio()
+	{
+        if (rb.velocity != Vector3.zero && source.clip != driving)
+        {
+            source.clip = driving;
+            source.Play();
+        }
+        else if(rb.velocity == Vector3.zero && source.clip != idle)
+		{
+            source.clip = idle;
+            source.Play();
+        }
+    }
+
+	private void CheckFiring()
+	{
+        if (Input.GetMouseButtonDown(0))
+            tankController.Fire();
+	}
+
+	private void GetInput()
+    {
+        movementInput = Input.GetAxis("Vertical");
+        rotationInput = Input.GetAxis("Horizontal");
+    }
+
+    public void SetTankController(TankController _tankController) => tankController = _tankController;
+
+    public Rigidbody GetRigidbody() => rb;
+
+    public void ChangeColor(Material color)
+    {
+        for (int i = 0; i < childs.Length; i++)
+        {
+            childs[i].material = color;
+        }
+    }
+}
